8c9b38da5f08f8a1face6578a1ad9f9059361cb3
MrR0B0T91 <dimabeastpv@gmail.com>
2022-04-26 08:13:32 +0600

    JDBC and Hibernate v1.1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -14,31 +14,31 @@ public class UserServiceImpl implements UserService {
 
     public void createUsersTable() {
 
-        userDaoHibernate.createUsersTable();
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
 
-        userDaoHibernate.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
 
-        userDaoHibernate.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
 
-        return userDaoHibernate.getAllUsers();
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
 
-        userDaoHibernate.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
     }
 }
