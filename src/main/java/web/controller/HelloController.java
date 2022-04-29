1d4068aabd75de9e961c716355c7dd4543c621f3
MrR0B0T91 <dimabeastpv@gmail.com>
2022-04-29 12:07:58 +0600

    ORM MVC Thymeleaf v1.1 @RequestParam

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
@@ -28,10 +29,9 @@ public class HelloController {
     }
 
     @GetMapping(value = "/cars")
-    public String cars(HttpServletRequest request, ModelMap model) {
+    public String cars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
 
-        if (request.getParameter("count") != null) {
-            int count = Integer.parseInt(request.getParameter("count"));
+        if (count != null) {
             list = carService.getCars(Car.creatList(), count);
         } else {
             list = Car.creatList();
